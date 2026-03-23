ba2cee9f88698c224d2d482bc2e3cc38a25590eb
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-23 13:46:46 +0545

    ui: update the hero section and add github svg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen bg-black items-center justify-center px-6">
-      <div className="mx-auto max-w-2xl text-center">
+    <main className="min-h-screen bg-black px-6">
+      <div className="mx-auto flex flex-col h-screen max-w-2xl items-center justify-center border-x border-neutral-700 text-center">
         <div className="mb-6 flex justify-center">
           <span className="text-muted-foreground rounded-full border border-neutral-500/30 px-4 py-1 text-sm">
             Powered by Servercn
@@ -21,18 +22,18 @@ export default function Page() {
 
         <div className="mt-8 flex items-center justify-center gap-4">
           <Link
-            href="https://servercn.vercel.app"
-            target="_blank"
-            className="rounded-md bg-black px-4 py-2.5 text-sm font-medium text-white hover:opacity-90 dark:bg-white dark:text-black">
-            Get Started
-          </Link>
-
-          <a
             href="https://github.com/akkaldhami/servercn"
             target="_blank"
-            className="hover:bg-blue-600 bg-blue-500 rounded-md px-4 py-2.5 text-sm font-medium">
-            Star on GitHub
-          </a>
+            className="flex items-center gap-2 rounded-full bg-white px-4 py-2.5 text-sm font-medium text-black hover:bg-neutral-300">
+            <Image src="/github.svg" alt="GitHub" width={20} height={20} /> Star
+            on GitHub
+          </Link>
+          <Link
+            href="https://servercn.vercel.app"
+            target="_blank"
+            className="rounded-full border border-neutral-400 bg-neutral-900 px-4 py-2.5 text-sm font-medium text-white hover:bg-neutral-800">
+            Get Started
+          </Link>
         </div>
       </div>
     </main>
